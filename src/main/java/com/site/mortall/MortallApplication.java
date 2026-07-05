9615827af9293baec1db8d874144d6eb11cb874d
mor tall <tallpape32@gmail.com>
2026-07-05 19:41:11 +0000

    Intialisation du projet

---
+++ src/main/java/com/site/mortall/MortallApplication.java
@@ -0,0 +1,13 @@
+package com.site.mortall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/site/mortall/MortallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.mortall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
